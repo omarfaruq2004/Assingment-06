e6fe87d75e332e19fb45752d8379b64530e9f0ec
unknown <omarfaruq669224@gmail.com>
2025-09-08 22:49:07 +0600

    Choose your trees section is complet

--- script.js
+++ script.js
@@ -43,23 +43,23 @@ const displayperbtn=(plants)=>{
     cardContainer.innerHTML="";
 
   plants.forEach(plant => {
-    // console.log(plant);
+//  console.log(plant);
 
 const plantBtn =document.createElement("div");
 plantBtn.innerHTML=`
 
-<div class="w-[344px] h-[381px] rounded-[8px] bg-[#CFF0DC]">
+<div class="w-[344px] max-h-[500px] rounded-[8px] bg-[#CFF0DC]">
 
-  <img src="./assets/about.png" class="w-[311px] h-[186px] rounded-[8px] pt-[16px] pl-[30px]" alt="">
-   <h1 class="pl-7 pt-4 font-semibold">Mango Tree</h1>
-   <p class="pl-7 pr-2 mt-2 text-[12px]">A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green</p>
+  <img src="${plant.image}" class="w-[311px] h-[186px]  pt-[16px] pl-[30px]" alt="">
+   <h1 class="pl-7 pt-4 font-semibold">${plant.name}</h1>
+   <p class="pl-7 pr-2 mt-2 text-[12px]">${plant.description}</p>
     
    <div class="flex justify-between px-7 mt-5">
-    <button class="w-[86px] h-[28px] bg-[#DCFCE7] rounded-3xl text-[#15803D] text-[12px]">Fruit Tree</button>
-    <h1><i class="fa-solid fa-bangladeshi-taka-sign"></i> 500</h1>
+    <button class="w-[150px] h-[28px] bg-[#DCFCE7] rounded-3xl text-[#15803D] text-[12px]">${plant.category}</button>
+    <h1><i class="fa-solid fa-bangladeshi-taka-sign"></i> ${plant.price}</h1>
    </div>
 
-   <button class="bg-[#15803D] w-[311px] h-[43px] rounded-3xl text-[#CFF0DC] ml-4 mt-2">Add to Cart</button>
+   <button class="bg-[#15803D] w-[311px] h-[43px] rounded-3xl text-[#CFF0DC] ml-4 mt-3 mb-5">Add to Cart</button>
 
 </div>
 
